ce51f676d484f258b5c9b159f8c80c7136c1a359
sug4rok <sug4rok@gmail.com>
2015-08-30 10:36:06 +0300

    Добавлен list_display плагинам в админке. Исправлена ошибка импорта в climate.cron

--- Servus/climate/admin.py
+++ Servus/climate/admin.py
@@ -1,14 +1,10 @@
 # coding=utf-8
 from django.contrib import admin
 from plugins.models import PLUGIN_MODELS
-from .models import TempHumidSensor
 
-
-class TempHumidSensorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'arduino_pin', 'location', 'location_type', 'is_used')
-    ordering = ('name',)
-
-
-admin.site.register(TempHumidSensor, TempHumidSensorAdmin)
 for plugin_model in PLUGIN_MODELS[__name__.split('.')[0]]:
-    admin.site.register(plugin_model)
+    PluginAdmin = type(plugin_model.__name__ + 'Admin', (admin.ModelAdmin, ), {
+        'list_display':('name', 'location', 'is_used'),
+        'ordering': ('name',),
+        })
+    admin.site.register(plugin_model, PluginAdmin)
--- Servus/system/admin.py
+++ Servus/system/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 from plugins.models import PLUGIN_MODELS
 
 for plugin_model in PLUGIN_MODELS[__name__.split('.')[0]]:
-    admin.site.register(plugin_model)
+    PluginAdmin = type(plugin_model.__name__ + 'Admin', (admin.ModelAdmin, ), {
+        'list_display':('name', 'location', 'is_used'),
+        'ordering': ('name',),
+        })
+    admin.site.register(plugin_model, PluginAdmin)
