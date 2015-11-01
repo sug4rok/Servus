7dab327c0b565804cf276c1c716d1713df6d4758
sug4rok <sug4rok@gmail.com>
2015-11-01 16:49:06 +0300

    Исправлена ошибка отображения виджета погоды

--- Servus/weather/widget.py
+++ Servus/weather/widget.py
@@ -23,12 +23,11 @@ def common_forecast(date):
     dt1 = datetime(date.year, date.month, date.day, 12)
     dt2 = datetime(date.year, date.month, date.day, 16)
     
-    # Получаем все модели плагинов типа 'Forecast'
+    # Получаем все используемые модели плагинов типа 'Forecast'
     f_objs = get_used_plugins_by_type('Forecast')
 
-    # Для каждой модели типа 'Forecast' получаем список объектов,учавствующих в усреднении
-    # (on_sidebar=True) и добавляем их в один кортеж.
-    sidebar_objs = reduce(lambda res, f: res + tuple(f.objects.filter(on_sidebar=True)), f_objs, ())
+    # Получаем кортеж объектов, учавствующих в усреднении (on_sidebar=True).
+    sidebar_objs = filter(lambda f: f.on_sidebar, f_objs)
 
     # Для каждого объекта forecast получаем последние данные из таблицы weather_weathervalue.
     w_objs = reduce(lambda res, f: res + tuple(WeatherValue.objects.filter(
