186236bd8f69c1096c89deb95304f54e200c5444
sug4rok <sug4rok@gmail.com>
2015-12-14 21:08:29 +0300

    Добавлено отображение в виде графика изменение атмосферного давления за последние трое суток

--- Servus/climate/views.py
+++ Servus/climate/views.py
@@ -5,27 +5,49 @@ from django.contrib.contenttypes.models import ContentType
 
 from base.views import call_template
 from plugins.utils import get_used_plugins_by
-from climate.models import TempHumidValue
+from climate.models import TempHumidValue, PressureValue
+
+
+def get_climate_data(sensors, ValueModel, attr):
+    """
+    Функция представляет определенные (параметр attr) климатические данные за последние
+    трое суток в нужном формате.
+    
+    :param sensors: list Список объектов климатических сенсоров.
+    :param ValueModel: object Модель для хранения климатических данных.
+    :param attr: str Атрибут объекта данных ('temperature', 'humidity', etc.)
+    :returns: list Список данных для отображения в виде графика на вкладке
+    Климат. Формат: [('расположение', ((дата1, значение1), (дата2, значение2), ...)]
+    """
+
+    values = ValueModel.objects.filter(datetime__gte=datetime.today() - timedelta(days=3)).order_by('datetime')
+
+    lst = []
+
+    for s in sensors:
+        s_v = values.filter(content_type_id=ContentType.objects.get_for_model(s).id, object_id=s.id)
+        lst.append((s.location, ((i.datetime, getattr(i, attr)) for i in s_v)))
+
+    return lst
 
 
 def climate(request):
     """
-    Функция отображения грификов данных с климатических датчиков
+    Функция вывода необходимых вкладке Климат данных с климатических датчиков, названий
+    единиц измерения, название вкладки и пр.
 
     :param request: django request
     """
 
     th_sensors = get_used_plugins_by(plugin_type='TempHumidSensor')
+    temps = get_climate_data(th_sensors, TempHumidValue, 'temperature')
+    humids = get_climate_data(th_sensors, TempHumidValue, 'humidity')
 
-    values = TempHumidValue.objects.filter(datetime__gte=datetime.today() - timedelta(days=3)).order_by('datetime')
+    p_sensors = get_used_plugins_by(plugin_type='PressureSensor')
+    pressures = get_climate_data(p_sensors, PressureValue, 'pressure')
 
-    temps, humids = [], []
-
-    for s in th_sensors:
-        s_v = values.filter(content_type_id=ContentType.objects.get_for_model(s).id, object_id=s.id)
-        temps.append((s.location, ((i.datetime, i.temperature) for i in s_v)))
-        humids.append((s.location, ((i.datetime, i.humidity) for i in s_v)))
-
-    params = {'active_app_name': 'climate', 'charts': (('температуры', '°C', temps), ('влажности', '%', humids))}
+    params = {'active_app_name': 'climate', 'charts': (('temp_div', 'температуры', '°C', temps),
+                                                       ('hum_div', 'влажности', '%', humids),
+                                                       ('press_div', 'атмосферного давления', 'мм рт.ст.', pressures))}
 
     return call_template(request, params)
